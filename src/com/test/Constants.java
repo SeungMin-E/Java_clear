1be678c0b7c746789322ca7b7190656741bdacbf
rainy2368 <rainy2368@gmail.com>
2023-06-19 17:29:22 +0900

    이거 해답 수정함. 다른건 다 맞은거 같은데 상수선언할때 public를 안넣었음

--- src/com/test/Constants.java
+++ src/com/test/Constants.java
@@ -1,6 +1,6 @@
 package com.test;
 
 public class Constants {
-	final static double PAI_1 = 3.14159;
-	final static double PAI_2 = 3.14159265358979323846;
+	public final static double PAI_1 = 3.14159;
+	public final static double PAI_2 = 3.14159265358979323846;
 }
